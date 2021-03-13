9fd5c7da1503bae2ec1befa17208c6b416a6aa74
Nerea García <nereagarcia.c@gmail.com>
2021-03-13 20:53:07 +0100

    crear componente Buscar

---
+++ src/components/Buscar.js
@@ -0,0 +1,12 @@
+
+const Buscar = (props) => {
+
+  return (
+    <label> Buscar:
+      <input type="text" className="form-control form-control-sm" />
+    </label >
+  );
+};
+
+
+export default Buscar;
