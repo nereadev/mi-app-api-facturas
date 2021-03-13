6fd39e9fdcc7125f98fb4a9c3a19b1fd710483f8
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-03-13 13:37:44 +0100

    import bootstrap y luxon

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
 
+import { DateTime } from "luxon";
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
 function App() {
   return (
     <>
-      <section class="principal container-fluid">
-        <header className="cabecera row">
-          <h2 className="col">Listado de ingresos</h2>
-        </header>
+      <Container as="section" className="principal" fluid={true}>
+        <Row as="header" className="cabecera">
+          <Col as="h2">Listado de ingresos</Col>
+        </Row>
         <main>
-          <div className="row">
-            <div className="info-listado info-listado-top col text-right">
-              <label>
-                Buscar
-            <input type="text" className="form-control form-control-sm">
-          </label>
-        </div>
-            </div>
-            <table className="listado table table-striped table-bordered table-hover">
-              <thead className="thead-light">
-                <tr>
-                  <th className="col-min">Num.</th>
-                  <th className="col-med">Fecha</th>
-                  <th className="col-concepto">Concepto</th>
-                  <th className="col-med">Base</th>
-                  <th className="col-max">IVA</th>
-                  <th className="col-med">Total</th>
-                  <th className="col-max">Estado</th>
-                  <th className="col-max">Vence</th>
-                </tr>
-              </thead>
-              <tbody className="lista-facturas">
-                <tr className="lista-dummy">
-                  <td className="numero">1000</td>
-                  <td className="fecha">13/06/2018</td>
-                  <td className="concepto">App Angular</td>
-                  <td className="base">3000€</td>
-                  <td className="iva">630€ (21%)</td>
-                  <td className="total">3630€</td>
-                  <td className="estado table-success">Abonada</td>
-                  <td className="vence table-success">-</td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th className="text-right" colspan="3">Totales:</th>
-                  <td className="base-total">5000€</td>
-                  <td className="iva-total">5000€</td>
-                  <td className="resultado-total">5000€</td>
-                  <td colspan="2"></td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
+          <Row >
+            <Col className="info-listado info-listado-top text-right">
+              <label>Buscar:
+              <input type="text" className="form-control form-control-sm" />
+              </label>
+            </Col>
+          </Row>
+          <table className="listado table table-striped table-bordered table-hover">
+            <thead className="thead-light">
+              <tr>
+                <th className="col-min">Num.</th>
+                <th className="col-med">Fecha</th>
+                <th className="col-concepto">Concepto</th>
+                <th className="col-med">Base</th>
+                <th className="col-max">IVA</th>
+                <th className="col-med">Total</th>
+                <th className="col-max">Estado</th>
+                <th className="col-max">Vence</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="factura factura-dummy">
+                <td className="numero"></td>
+                <td className="fecha"></td>
+                <td className="concepto"></td>
+                <td><span className="base"></span>€</td>
+                <td><span className="cantidad-iva"></span>€ (<span className="tipo-iva"></span>%)</td>
+                <td><span className="total"></span>€</td>
+                <td className="estado"></td>
+                <td className="vencimiento"></td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr className="totales">
+                <th className="text-right" colSpan="3">Totales:</th>
+                <td><span className="total-bases"></span>€</td>
+                <td><span className="total-ivas"></span>€</td>
+                <td><span className="total-totales"></span>€</td>
+                <td colSpan="2"></td>
+              </tr>
+            </tfoot>
+          </table>
         </main>
-      </section>
+      </Container>
       <div className="loading off">
         <img src="img/loading.svg" alt="cargando" />
       </div>
